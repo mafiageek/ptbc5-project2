8b4ab5a30374f309f45b5b1406d7667520faaa9f
mafiageek <anton_kho@msn.com>
2022-11-19 12:53:27 +0800

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import About from "./pages/About";
+import Listings from "./pages/Listings";
+import SubmitRequest from "./pages/SubmitRequest";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/About" element={<About />} />
+        <Route path="/SubmitRequest" element={<SubmitRequest />} />
+        <Route path="/Listings" element={<Listings />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/SubmitRequest.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SubmitRequest() {
+  return <div>SubmitRequest</div>;
+}
+
+export default SubmitRequest;
