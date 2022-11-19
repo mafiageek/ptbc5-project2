46393eb8c1eab2c11fbbbd678b1eb4ebc03d5e08
mafiageek <anton_kho@msn.com>
2022-11-19 23:15:21 +0800

    added google auth

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { auth } from "../firebase";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { Button, Typography } from "@mui/material";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
+
+  const handleLogin = async () => {
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("User", currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <div>
+      NavBar
+      {!user?.email ? (
+        <Button variant="contained" onClick={handleLogin}>
+          Login
+        </Button>
+      ) : (
+        <>
+          <Typography>{user?.displayName}</Typography>
+          <Button variant="contained" onClick={handleLogout}>
+            Logout
+          </Button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default NavBar;
