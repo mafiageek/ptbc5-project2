a5cafd3d4b15a4c3c90e7206de353c08b3e7a2db
Weimankow <weimankow@gmail.com>
2022-11-22 12:20:03 +0800

    created basic color theming - primary, secondary, info colors changed

--- src/App.js
+++ src/App.js
@@ -1,54 +1,66 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-// import {createMuiTheme, ThemeProvider} from "@material-ui/core"
-import {AppBar, Toolbar, Container, Typography, Button }from "@mui/material"
+import { ThemeProvider, createTheme } from "@mui/material";
+import { AppBar, Toolbar, Container, Typography, Button } from "@mui/material";
 import About from "./pages/About";
 import Listings from "./pages/Listings";
 import SubmitRequest from "./pages/SubmitRequest";
-import NavBar from "./components/NavBar.js"
+import NavBar from "./components/NavBar.js";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#8B93FB",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#012D48",
+    },
 
-// const theme = createMuiTheme({
-// palette: {
-//   primary: {
-//     main: "#8B93FB"
-//   }
-// }
-
-// })
-
+    info: {
+      main: "#f7d626",
+    },
+  },
+});
 
 function App() {
   return (
-<>
-<BrowserRouter>
-    <AppBar>
-      <Toolbar>
-    
-   <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            MAKING MEANING
-          </Typography>
-          <Button variant="text" variant="h5" >  About
-</Button>
+    <>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <AppBar>
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                MAKING MEANING
+              </Typography>
+              <Button variant="text" variant="h5">
+                {" "}
+                About
+              </Button>
 
-          <Button variant="text" variant="h5" > Submit Request
-</Button>
+              <Button variant="text" variant="h5">
+                {" "}
+                Submit Request
+              </Button>
 
-          <Button variant="text" variant="h5" >Volunteer Opportunities</Button>
+              <Button variant="text" variant="h5">
+                Volunteer Opportunities
+              </Button>
 
-
-
-           <Button variant="contained">Login</Button>          
-          </Toolbar>
+              <Button variant="contained" color="secondary">
+                Login
+              </Button>
+            </Toolbar>
           </AppBar>
 
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Listings />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/SubmitRequest" element={<SubmitRequest />} />
-      </Routes>
-    </BrowserRouter>
+          {/* <NavBar /> */}
+          <Routes>
+            <Route path="/" element={<Listings />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/SubmitRequest" element={<SubmitRequest />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
