f0eaf4b164bba96b01621600a646d5c9eb22c3bb
mafiageek <anton_kho@msn.com>
2022-12-01 14:50:10 +0800

    added sendgrid and sample smtp code

--- src/pages/SubmitRequest.js
+++ src/pages/SubmitRequest.js
@@ -47,6 +47,7 @@ function SubmitRequest(props) {
     timestamp: "",
     dueDate: "",
     uid: "",
+    replyTo: "",
   });
 
   const [logo, setLogo] = React.useState(null);
@@ -89,10 +90,31 @@ function SubmitRequest(props) {
               mapURL: response.config.url,
               dueDate: selectedDate.toDateString(),
               uid: props.user.uid,
+              replyTo: props.user.email,
               timestamp: serverTimestamp(),
             });
           });
         });
+      });
+
+    const options = {
+      method: "POST",
+      url: "https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": process.env.REACT_APP_SENDGRID_API_KEY,
+        "X-RapidAPI-Host": "rapidprod-sendgrid-v1.p.rapidapi.com",
+      },
+      data: '{"personalizations":[{"to":[{"email":"anton.kho@gmail.com"}],"subject":"Hello, World!"}],"from":{"email":"from_address@example.com"},"content":[{"type":"text/plain","value":"Hello, World!"}]}',
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
       });
   };
 
